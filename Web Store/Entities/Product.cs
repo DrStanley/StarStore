ec1bff0dab514c37bc3d0616532d63781639cc51
Ozoezi stanley chukwujekwu <43678258+DrStanley@users.noreply.github.com>
2020-09-22 15:35:27 +0100

    Add project files.

---
+++ Web Store/Entities/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_Store.Entities
+{
+	public class Product
+	{
+        [ScaffoldColumn(false)]
+
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Item Name")]
+        public string ProductName { get; set; }
+
+        [Required, Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public decimal? UnitPrice { get; set; }
+
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
