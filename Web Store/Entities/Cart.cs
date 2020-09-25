d0a5d804bef3ab070365683338a32c388e254201
Ozoezi stanley chukwujekwu <43678258+DrStanley@users.noreply.github.com>
2020-09-25 22:24:21 +0100

    user

--- Web Store/Entities/Cart.cs
+++ Web Store/Entities/Cart.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using Web_Store.Models;
 
 namespace Web_Store.Entities
 {
 	public class Cart
 	{
         [Key]
+
         public string CartId { get; set; }
 
         public int Quantity { get; set; }
@@ -18,6 +20,10 @@ namespace Web_Store.Entities
         public int ProductId { get; set; }
 
         public virtual Product Product { get; set; }
+
+        public string UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
 
     }
 }
